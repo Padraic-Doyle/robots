239532138cd82c536b8c687fa484e5c37303d61b
padraic <high5developer@gmail.com>
2019-03-22 10:39:47 +0000

    added componentDidMount and fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Cardlist from './Cardlist';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -11,9 +10,15 @@ class App extends Component  {
     constructor(){
         super()
         this.state ={
-            robots: robots,
+            robots: [],
             searchfield:''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}))
     }
 
   onSearchChange =(event) => {
